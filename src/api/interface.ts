d27e0aae534e3e9832a5e069f962b1a9f12ce072
wqh <easylivee@163.com>
2023-07-05 10:23:12 +0800

    ..

--- src/api/interface.ts
+++ src/api/interface.ts
@@ -3,3 +3,17 @@ import axios from 'axios';
 export const getBedList = (params?: any) => {
   return axios.post('/admin/bed_list', params);
 };
+
+//#在 interface 里面，定义接口，在页面中
+/* import { getBedList } from '@/api/interface.ts'; 使用接口
+在代码里面 const getBed = async () => {
+const params = {
+current: current,
+pageSize: pageSize,
+search: bedSer,
+};
+const data = await getBedList(params);
+setList(data.data);
+setTotal(data.total.total);
+};
+这样使用 */
